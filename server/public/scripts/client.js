8cef0adc9cc55c78981898e29adfe29627cbc8a7
Shyla Earl <shyla.earl@gmail.com>
2020-10-17 11:30:41 -0500

    all static files and sourcing functional

---
+++ server/public/scripts/client.js
@@ -0,0 +1,25 @@
+console.log('js');
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('JQ');
+    //add page load function
+    //add click listeners
+}
+
+//function getTask(){
+
+//}
+
+//function postTask(){
+
+//}
+
+//function deleteTask(){
+
+//}
+
+//function editTask(){
+
+//}
---
+++ server/routes/task_router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+// define an object array with dummy data
+
+//router.get
+
+//router.post
+
+//router.delete
+
+//router.put
+
+module.exports = router;
