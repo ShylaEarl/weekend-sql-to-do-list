6b3c8b664cc674e4b142133879d0ca3d7df38bc8
Shyla Earl <shyla.earl@gmail.com>
2020-10-17 14:02:10 -0500

    GET functional and add task click event working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,17 +4,59 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
-    //add page load function
-    //add click listeners
+    //on page load add task list
+    getTask();
+    // //add click listeners
+    $('#addTask').on('click', postTask);
+    //$(#todoTableBody).on('click', '.deleteButton', deleteTask);
+    // $(#todoTableBody).on('click', '.editTask', editTask);
 }
 
-//function getTask(){
+//get task data from server
+function getTask(){
+    //$("#todoTableBody").empty();
+    $.ajax({
+        method: 'GET',
+        url: '/taskList'
+    }).then(function (response) {
+        console.log('in client get request', response);
+        // append data to DOM
+        for (let i = 0; i < response.length; i++) {
+            $('#todoTableBody').append(`
+                <tr>
+                    <td>${response[i].name}</td>
+                    <td>${response[i].task}</td>
+                    <td>${response[i].date}</td>
+                    <td>${response[i].completed}</td>
+                    <td><button data-id=${response[i].id} class=deleteButton>Delete</button></td>
+                </tr>
+            `);
+        }    
+    });
+}
 
-//}
+//<td><button data-id=${response[i].id} data-direction='up' class='completed'>Completed</button>
+//<button data-id=${response[i].id} data-direction='down' class='rankDown'>DOWN</button></td>
 
-//function postTask(){
-
-//}
+function postTask(){
+    let taskListObject = {
+        name: $('#name').val(),
+        task: $('#task').val(),
+        date: $('#date').val(),
+        completed: $('#completed').val()
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/taskList',
+        data: taskListObject
+    }).then( function (response) {
+        $('#artist').val(''),
+        $('#track').val(''),
+        $('#rank').val(''),
+        $('#published').val('')
+        getTask();
+    });
+}
 
 //function deleteTask(){
 
--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -5,6 +5,17 @@ const pool = require('../modules/pool');
 // define an object array with dummy data
 
 //router.get
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "tasks";`;
+    pool.query(queryText)
+        .then((result) => {
+        res.send(result.rows);
+        }).catch((error) => {
+            console.log(`Error making query ${queryText}`, error);
+            res.sendStatus(500);
+        });  
+    
+});
 
 //router.post
 
