b035c538d2e6d9814cf039c8db069991c5535d93
Shyla Earl <shyla.earl@gmail.com>
2020-10-20 18:34:46 -0500

    changed pool module for Heroku deployment

--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool');
+var pool = require('../modules/pool.js'); //const pool
 
 // could add an object array with dummy data
 
