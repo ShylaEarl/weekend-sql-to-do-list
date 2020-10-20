a6934cb267a1322078aac05ea49670d754c41f19
Shyla Earl <shyla.earl@gmail.com>
2020-10-20 08:59:41 -0500

    changes preparing for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use('/taskList', taskRouter);
 
 
 // Start express
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('up and running on port', PORT);
 });
